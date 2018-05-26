25458d1b3fc2a2d74063356a248655e373e1b6c9
levect123 <verdugo.leandro@outlook.es>
2018-05-26 13:10:31 -0400

    joder

---
+++ ejemplo.py
@@ -0,0 +1,9 @@
+import os, sys
+a = int(input("primer numero :"))
+b = int(input("segundo numero :"))
+c = int(input("tercer numero :"))
+d = int(input("cuarto numero :"))
+
+total = a+b+c+d
+mensaje = "a pagar"
+print(mensaje,total)
